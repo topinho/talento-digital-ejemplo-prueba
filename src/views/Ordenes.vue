be3332373bf85f862b58660d5b471362458720b6
German <topinho1806@gmail.com>
2021-04-03 21:41:26 -0300

    commit 9. Agregar Tabla Ordenes. api call ordenes.

--- src/views/Ordenes.vue
+++ src/views/Ordenes.vue
@@ -1,15 +1,59 @@
 <template>
   <div class="container">
-    <h1>Esta es la pagina de Ordenes</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Cliente</th>
+          <th scope="col">Monto</th>
+          <th scope="col">Cantidad</th>
+          <th scope="col">Fecha</th>
+          <th scope="col">Avance Preparacion</th>
+          <th scope="col">Estado</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="o in ordenes" :key="o.num_orden">
+          <th>{{ o.num_orden }}</th>
+          <th>{{ o.cliente }}</th>
+          <th>{{ o.monto }}</th>
+          <th>{{ o.cant_productos }}</th>
+          <th>{{ o.fecha_entrega }}</th>
+          <th>{{ o.avance_preparacion }}</th>
+          <th>{{ o.estado }}</th>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'Ordenes',
   components: {
-
+  },
+  data () {
+    return {
+      ordenes: [],
+      mensaje: '',
+      api: 'http://localhost:3911'
+    }
+  },
+  created() {
+    this.obtener_ordenes()
+  },
+  methods: {
+    obtener_ordenes() {
+      axios.get(this.api + '/ordenes')
+      .then(
+        respuesta => {
+          console.log(respuesta)
+          this.ordenes = respuesta.data
+        }
+      )
+    }
   }
 }
 </script>
